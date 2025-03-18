9a8dde99d76eb51c225ef40fc818e7322a4ec4c2
fadilsyy <fadilasytar@gmail.com>
2025-03-18 20:12:16 +0700

    perulangan while

--- file2.cpp
+++ file2.cpp
@@ -22,4 +22,15 @@ int main(){
     
     cout << "Nilai x terakhir = " << x << endl;
     cout << "Jumlah Perulangan = " << perulanganWhile << endl;
+
+    //Perulangan Do While
+    cout << endl;
+    cout << "Perulangan Do While " << endl;
+    do{
+        cout << "Nilai x = " << x <<" Lari Keliling " << endl;
+        x = rand() % 10;
+        perulanganDo++;
+    }while(x < 7);
+    cout << "Nilai x terakhir : " << x << endl;
+    cout << "Jumlah Perulangan Do While : " << perulanganDo << endl;
 }
